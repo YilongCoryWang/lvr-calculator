db36765dd84dc64fc787c5daa81a53ad08578cdc
yilong <cory_wng@yahoo.com>
2025-01-30 18:21:45 +1100

    [Yilong] feat: add swagger and API doc

--- lvr-backend/src/routes/v1/lvrRouter.ts
+++ lvr-backend/src/routes/v1/lvrRouter.ts
@@ -6,6 +6,67 @@ import {
   uploadPropertyValueEvidence,
 } from "../../controller/lvrController";
 
+/**
+ * @openapi
+ * /lvr:
+ *   get:
+ *     description: get loan to value ratio
+ *     parameters:
+ *       - name: estPropValue
+ *         description: estimated property value
+ *         in: query
+ *         required: true
+ *         type: string
+ *       - name: estLoanValue
+ *         description: estimated loan value
+ *         in: query
+ *         required: true
+ *         type: string
+ *       - name: cashOutAmt
+ *         description: cash out amount
+ *         in: query
+ *         required: false
+ *         type: string
+ *       - name: propValuePhy
+ *         description: property value (physical)
+ *         in: query
+ *         required: false
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: success and return lvr
+ *   post:
+ *     description: create loan to value ratio and upload document of property value (physical) evidence
+ *     parameters:
+ *       - name: estPropValue
+ *         description: estimated property value
+ *         in: body
+ *         required: true
+ *         type: string
+ *       - name: estLoanValue
+ *         description: estimated loan value
+ *         in: body
+ *         required: true
+ *         type: string
+ *       - name: cashOutAmt
+ *         description: cash out amount
+ *         in: body
+ *         required: false
+ *         type: string
+ *       - name: propValuePhy
+ *         description: property value (physical)
+ *         in: body
+ *         required: false
+ *         type: string
+ *       - name: evidence
+ *         description: document of property value (physical) evidence
+ *         in: body
+ *         required: false (true if propValuePhy exists)
+ *         type: file
+ *     responses:
+ *       200:
+ *         description: success and return lvr
+ */
 const router = Router();
 router
   .route("/lvr")
--- lvr-backend/src/server.ts
+++ lvr-backend/src/server.ts
@@ -1,11 +1,27 @@
 import express from "express";
 import cors from "cors";
 import lvrRouter from "./routes/v1/lvrRouter";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerui from "swagger-ui-express";
 
 const app = express();
+
 app.use(cors());
 app.use(express.json({ limit: "10kb" }));
 app.use("/api/v1", lvrRouter);
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Loan to Value Calculator",
+      version: "1.0.0",
+    },
+  },
+  apis: ["./src/routes/v1/*.ts"], // files containing annotations as above
+};
+const openapiSpecification = swaggerJsdoc(options);
+app.use("/api-docs", swaggerui.serve, swaggerui.setup(openapiSpecification));
 app.all("*", (req, res, next) => {
   res
     .status(404)
